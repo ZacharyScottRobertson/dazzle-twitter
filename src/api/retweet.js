2336f9a93881a01bd617ec006f05adb9a04fa568
Zack Robertson <zackisthebestever@outlook.com>
2018-08-10 11:58:01 -0700

    Commiting updated code lines

--- src/api/retweet.js
+++ src/api/retweet.js
@@ -1,57 +1,44 @@
 const Twit = require('twit')
-const unique = require('unique-random-array')
+
 const config = require('../config')
-const isReply = require('../helpers/isReply')
+
 
 const param = config.twitterConfig
 //const queryString = unique(param.queryString.split(','))
 
-const bot = new Twit(config.twitterKeys)
+const Twitter = new Twit(config.twitterKeys)
 
-const retweet = () => {
-  const query = '#MedicalMarijuana, #Cannabis, #LegalizedCannabis, #CannabisNews, #Cannabiz, #Cannabizness, #Marijuana, #MMJ, #CBD, #CannabisLaw, #Hemp, #CannabisBusiness, #Dispensary'
+var retweet = function() {
+  var params = {
+      q: '#MMJ, #CBD',  // REQUIRED
+      result_type: 'recent',
+      lang: 'en'
+  }
+  // for more parametes, see: https://dev.twitter.com/rest/reference/get/search/tweets
 
-  bot.get(
-    'search/tweets',
-    {
-      q: query,
-      result_type: 'popular',
-      lang: param.language,
-      filter: 'safe',
-      count: param.searchCount
-    },
-    (err, data, response) => {
-      if (err) {
-        console.lol('ERRORDERP: Cannot Search Tweet!, Description here: ', err)
-      } else {
-        // grab random tweet ID to retweet - desired range for random number is [0..data.statuses.length-1]
-        const rando = Math.floor(Math.random() * data.statuses.length)
-        let retweetId
-
-        if (!isReply(data.statuses[rando])) {
-          retweetId = data.statuses[rando].id_str
-        }
-
-        bot.post(
-          'statuses/retweet/:id',
-          {
-            id: retweetId
-          },
-          (err, response) => {
-            if (err) {
-              console.lol('ERRORDERP: Retweet!')
-            }
-            console.lol(
-              'SUCCESS: RT: ',
-              data.statuses[rando].text,
-              'RANDO ID: ',
-              rando
-            )
-          }
-        )
+  Twitter.get('search/tweets', params, function(err, data) {
+    // if there no errors
+      if (!err) {
+        // grab ID of tweet to retweet
+          var retweetId = data.statuses[0].id_str;
+          // Tell TWITTER to retweet
+          Twitter.post('statuses/retweet/:id', {
+              id: retweetId
+          }, function(err, response) {
+              if (response) {
+                  console.log('Retweeted!!!');
+              }
+              // if there was an error while tweeting
+              if (err) {
+                  console.log('Something went wrong while RETWEETING... Duplication maybe...');
+              }
+          });
       }
-    }
-  )
+      // if unable to Search a tweet
+      else {
+        console.log('Something went wrong while SEARCHING...', err);
+      }
+});
 }
 
 module.exports = retweet
